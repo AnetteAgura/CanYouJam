59d0b5c5e7e1323570791897450a4c60436ad41d
Anette Aguraiuja <anetteagura@gmail.com>
2021-03-13 13:27:33 +0200

    Victory screen

---
+++ Assets/Codes/ShowScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour
+{
+    public Text countText;
+    public Image youWin;
+    public Text youWinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + StaticScore.score.ToString();
+
+        if (StaticScore.score >= 200)
+        {
+            youWin.enabled = true;
+            youWinText.enabled = true;
+        }
+    }
+}
