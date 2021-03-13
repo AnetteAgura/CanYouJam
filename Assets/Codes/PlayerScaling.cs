c8f8815be31f660d4b3e7f8cf539b9068fb2c879
Anette Aguraiuja <anetteagura@gmail.com>
2021-03-13 18:43:57 +0200

    Added function, not a bug thingy, anti-virus don't move but with enough points yu can destroy them.

--- Assets/Codes/PlayerScaling.cs
+++ Assets/Codes/PlayerScaling.cs
@@ -9,6 +9,12 @@ public class PlayerScaling : MonoBehaviour
     private Vector3 scaleChange, scaleChange1;
     private int timer = 0;
 
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Awake()
     {
@@ -16,26 +22,85 @@ public class PlayerScaling : MonoBehaviour
         scaleChange1 = new Vector3(1.5f, 1.5f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+     void Update()
     {
-        if(StaticScore.score == 20 && timer == 0) 
+        StartCoroutine(Scaling());
+    }
+    // Update is called once per frame
+    IEnumerator Scaling()
+    {
+
+        if (StaticScore.score == 20 && timer == 0)
         {
+            anim.SetTrigger("Scale");
             Debug.Log("ScaleUp!");
-            player.transform.localScale += scaleChange;
             timer += 1;
+
+            yield return new WaitForSeconds(1);
+
+            player.transform.localScale += scaleChange;
+            anim.SetTrigger("Play");
         }
 
         if (StaticScore.score >= 50 && timer == 1)
         {
-            player.transform.localScale += scaleChange1;
+            anim.SetTrigger("Scale");
+            Debug.Log("ScaleUp!");
             timer += 1;
+
+            yield return new WaitForSeconds(1);
+
+            player.transform.localScale += scaleChange1;
+            anim.SetTrigger("Play");
         }
 
         if (StaticScore.score >= 100 && timer == 2)
         {
-            player.transform.localScale += scaleChange1;
+            anim.SetTrigger("Scale");
+            Debug.Log("ScaleUp!");
             timer += 1;
+
+            yield return new WaitForSeconds(1);
+
+            player.transform.localScale += scaleChange1;
+            anim.SetTrigger("Play");
         }
+
+        if (StaticScore.score <= 19 && timer == 1)
+        {
+            anim.SetTrigger("Scale");
+            Debug.Log("ScalingDown!");
+            timer -= 1;
+
+            yield return new WaitForSeconds(1);
+
+            player.transform.localScale -= scaleChange;
+            anim.SetTrigger("Play");
+        }
+
+        if (StaticScore.score <= 49 && timer == 2)
+        {
+            anim.SetTrigger("Scale");
+            Debug.Log("ScalingDown!");
+            timer -= 1;
+
+            yield return new WaitForSeconds(1);
+
+            player.transform.localScale -= scaleChange1;
+            anim.SetTrigger("Play");
+        }
+
+        if (StaticScore.score <= 99 && timer == 3)
+        {
+            anim.SetTrigger("Scale");
+            Debug.Log("ScalingDown!");
+            timer -= 1;
+
+            yield return new WaitForSeconds(1);
+
+            player.transform.localScale -= scaleChange1;
+            anim.SetTrigger("Play");
+        }
+    
     }
 }
